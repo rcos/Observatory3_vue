3b95e1a88502d85b7ae8d7bb51c4a03c5fc192e6
Alexander Schwartzberg <aeksco@gmail.com>
2017-11-19 15:21:26 -0500

    Refined project list - pined component

--- src/containers/project_list/components/pinned.vue
+++ src/containers/project_list/components/pinned.vue
@@ -4,29 +4,44 @@
 
 
       <div class="card">
-        <h6 class="card-header">My projects</h6>
+        <h6 class="card-header">
+          <i class="fa fa-fw fa-folder-open-o"></i>
+          My Projects
+        </h6>
         <div class="card-body pt-1 pb-1">
           <p class="card-text">
             <dl class="dl-horizontal">
-              <dd>Observatory</dd>
-              <dd>AstroKey</dd>
+              <dd>
+                <a href="/#/">Observatory</a>
+              </dd>
+              <dd>
+                <a href="/#/">YACS</a>
+              </dd>
             </dl>
           </p>
         </div>
       </div>
 
       <div class="card mt-3">
-        <h6 class="card-header">Favorite Projects</h6>
+        <h6 class="card-header">
+          <i class="fa fa-fw fa-star-o"></i>
+          Starred
+        </h6>
         <div class="card-body pt-1 pb-1">
           <p class="card-text">
             <dl class="dl-horizontal">
-              <dd>Observatory</dd>
+              <dd>
+                <a href="/#/">Observatory</a>
+              </dd>
             </dl>
           </p>
         </div>
       </div>
 
-      <button type="button" class='btn btn-outline-secondary w-100 mt-3'>Past Projects</button>
+      <button type="button" class='btn btn-outline-secondary w-100 mt-3'>
+        <i class="fa fa-fw fa-backward mr-1"></i>
+        Past Projects
+      </button>
 
     </div>
   </div>
