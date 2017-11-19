3ae92ef81702358027d4c80dad4ca6230d754eeb
Alexander Schwartzberg <aeksco@gmail.com>
2017-11-19 17:53:08 -0500

    UserProfile - split sidebar into standalone component

--- src/containers/user_show/components/layout.vue
+++ src/containers/user_show/components/layout.vue
@@ -6,26 +6,7 @@
     <div class="row">
 
       <div class="col-lg-3">
-
-        <div class="card">
-          <div class="card-body">
-            <img class="profile-img w-100" :src="model.avatar"/>
-            <h4 class='card-title text-center pt-3 mb-0'>{{model.name}}</h4>
-          </div>
-
-          <ul class="list-group list-group-flush">
-            <a class='list-group-item' :href=" 'https://github.com/' + model.githubProfile ">
-              <i class="fa fa-fw fa-github"></i>
-              {{model.githubProfile}}
-            </a>
-
-            <a class='list-group-item' :href=" 'mailto:' + model.email ">
-              <i class="fa fa-fw fa-envelope-o"></i>
-              {{model.email}}
-            </a>
-          </ul>
-
-        </div>
+        <Sidebar :model="model"/>
       </div>
 
       <div class="col-lg-9">
@@ -41,19 +22,12 @@
 <!-- // // // //  -->
 
 <script>
+import Sidebar from './sidebar'
 import Bio from './bio'
 import Projects from './projects'
 
 export default {
   props: ['model'],
-  components: { Projects, Bio }
+  components: { Sidebar, Projects, Bio }
 }
 </script>
-
-
-<style type="text/css">
-  .profile-img {
-    border-radius: 50%;
-    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
-  }
-</style>
---
+++ src/containers/user_show/components/sidebar.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <img class="profile-img w-100" :src="model.avatar"/>
+      <h4 class='card-title text-center pt-3 mb-0'>{{model.name}}</h4>
+    </div>
+
+    <ul class="list-group list-group-flush">
+      <a class='list-group-item' :href=" 'https://github.com/' + model.githubProfile ">
+        <i class="fa fa-fw fa-github"></i>
+        {{model.githubProfile}}
+      </a>
+
+      <a class='list-group-item' :href=" 'mailto:' + model.email ">
+        <i class="fa fa-fw fa-envelope-o"></i>
+        {{model.email}}
+      </a>
+    </ul>
+
+  </div>
+</template>
+
+<!-- // // // //  -->
+
+<script>
+export default {
+  props: ['model']
+}
+</script>
+
+<style type="text/css">
+  .profile-img {
+    border-radius: 50%;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
+  }
+</style>
