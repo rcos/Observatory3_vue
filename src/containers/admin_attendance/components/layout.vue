0fec01a945367ccb8e08433b211fb25f38759d31
Frankie Guzikowski <frankieguzikowski@dcc-wl-96.dynamic2.rpi.edu>
2017-11-19 17:27:08 -0500

    Copied layout format from mentor_verify

---
+++ src/containers/admin_attendance/components/layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h2>Mentor - Verify</h2>
+    <hr>
+    <p class="lead">TODO</p>
+  </div>
+</template>
+
+<!-- // // // //  -->
+
+<script>
+export default {
+  name: 'layout'
+}
+</script>
