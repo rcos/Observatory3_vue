471684142da3e241867ca03f2102835589b72b53
Frankie Guzikowski <frankieguzikowski@dcc-wl-96.dynamic2.rpi.edu>
2017-11-19 23:37:23 -0500

    Copied table from admin_list

---
+++ src/containers/admin_users/components/list.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <table class="table table-hover table-striped">
+    <thead>
+      <tr>
+        <th scope="col">Info</th>
+        <th scope="col">Full Group</th>
+        <th scope="col">Small Group</th>
+        <th scope="col">Contributors</th>
+        <th scope="col">Edit</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">Aug 31</th>
+        <td>Bonus</td>
+        <td>77</td>
+        <td>Delete</td>
+      </tr>
+      <tr>
+        <th scope="row">Sep 04</th>
+        <td>Regular</td>
+        <td>100</td>
+        <td>Delete</td>
+      </tr>
+      <tr>
+        <th scope="row">Sep 07</th>
+        <td>Regular</td>
+        <td>122</td>
+        <td>Delete</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
