bb81dcb000cfd81252254f33cff524a9f5a821a4
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-11 16:12:50 -0500

    App authenticates on start

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,36 @@
 
 <template>
   <div id="app">
+    <Notification/>
     <AppNavbar/>
     <router-view/>
     <AppFooter/>
-    <Notification/>
   </div>
 </template>
 
 <script>
-import AppNavbar from './containers/app_navbar'
-import AppFooter from './containers/app_footer'
-import Notification from './containers/app_notification'
+import AppNavbar from '@/containers/app_navbar'
+import AppFooter from '@/containers/app_footer'
+import Notification from '@/containers/app_notification'
 
 export default {
   name: 'app',
-
-  // Top-Level Application Components
   components: {
     AppNavbar,
     AppFooter,
     Notification
   },
-
-  // Top-Level page Meta
   metaInfo: {
-    title: 'Home', // set a title
+    title: 'Lodaing...',
     titleTemplate: 'RCOS - %s', // title will always be "RCOS - ..."
     htmlAttrs: {
       lang: 'en'
     }
+  },
+  created () {
+    // Authenticates on application start
+    this.$store.dispatch('auth/fetchUserProfile')
   }
-
 }
 </script>
 
