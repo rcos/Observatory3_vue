8a95adc1c652600894e059784cab6ede52a92fa2
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-11 16:14:25 -0500

    Navbar integrated auth module

--- src/containers/app_navbar/components/layout.vue
+++ src/containers/app_navbar/components/layout.vue
@@ -1,6 +1,6 @@
 <template>
 	<nav class="navbar navbar-expand-lg navbar-light">
-	  <a class="navbar-brand" href="/#/">RCOS</a>
+	  <a class="navbar-brand" href="#/">RCOS</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
@@ -8,58 +8,72 @@
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	    <ul class="navbar-nav mr-auto">
 	      <li class="nav-item">
-          <a class="nav-link" href="/#/projects">Projects</a>
+          <a class="nav-link" href="#/projects">Projects</a>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="/#/users">Developers</a>
+          <a class="nav-link" href="#/users">Developers</a>
         </li>
 
         <li>
-          <a class="nav-link" href="/#/blogs">Blogs</a>
+          <a class="nav-link" href="#/blogs">Blogs</a>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="/#/achievements">Achievements</a>
+          <a class="nav-link" href="#/achievements">Achievements</a>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="/#/sponsors">Sponsors</a>
+          <a class="nav-link" href="#/sponsors">Sponsors</a>
         </li>
 
         <li>
-          <a class="nav-link" href="/#/main/attend">Attend</a>
+          <a class="nav-link" href="#/main/attend">Attend</a>
         </li>
 
         <b-nav-item-dropdown text="Mentor" right>
-          <b-dropdown-item href="/#/mentor/verify">Verify</b-dropdown-item>
-          <b-dropdown-item href="/#/mentor/small_group">Small Group</b-dropdown-item>
+          <b-dropdown-item href="#/mentor/verify">Verify</b-dropdown-item>
+          <b-dropdown-item href="#/mentor/small_group">Small Group</b-dropdown-item>
         </b-nav-item-dropdown>
 
         <b-nav-item-dropdown text="Admin" right>
-          <b-dropdown-item href="/#/admin/attendance">Attendance</b-dropdown-item>
-          <b-dropdown-item href="/#/admin/users">Users</b-dropdown-item>
-          <b-dropdown-item href="/#/admin/settings">Settings</b-dropdown-item>
+          <b-dropdown-item href="#/admin/attendance">Attendance</b-dropdown-item>
+          <b-dropdown-item href="#/admin/users">Users</b-dropdown-item>
+          <b-dropdown-item href="#/admin/settings">Settings</b-dropdown-item>
         </b-nav-item-dropdown>
 
 	    </ul>
 
-	    <ul class="navbar-nav ml-auto">
-	      <li class="nav-item">
-	        <a class="nav-link" href="/#/auth/register">Register</a>
-	      </li>
-	      <li class="nav-item">
-	        <a class="nav-link" href="/#/auth/login">Login</a>
-	      </li>
+      <!-- TODO - build this out as a dropdown menu for the user -->
+      <ul class="navbar-nav ml-auto" v-if="isAuthenticated">
+        <li class="nav-item">
+          <a class="nav-link" href="#/profile">{{ currentUser.email }}</a>
+        </li>
 	    </ul>
+
+      <!-- Register / Login -->
+      <ul class="navbar-nav ml-auto" v-else>
+        <li class="nav-item">
+          <a class="nav-link" href="#/auth/register">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#/auth/login">Login</a>
+        </li>
+      </ul>
+
 	  </div>
 	</nav>
 </template>
 
 <script>
-// TODO - this should be split into a series of smaller components
+// TODO - this file should be split into a series of smaller components
+import { mapGetters } from 'vuex'
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  computed: mapGetters({
+    isAuthenticated: 'auth/is_authenticated',
+    currentUser: 'auth/current_user'
+  })
 }
 </script>
 
