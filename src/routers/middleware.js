1e0d3f2280dce06fa1389f31b903ccfb36c62fe6
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-11 19:00:13 -0500

    EnsureAuthenticated router middleware tightened up

--- src/routers/middleware.js
+++ src/routers/middleware.js
@@ -14,15 +14,20 @@ const requireAuth = (route, redirect, next) => {
 
 const ensureAuthenticated = (route, redirect, next) => {
   // Pulls currentUser from Auth module store
+  let token = store.getters['auth/token']
   let currentUser = store.getters['auth/current_user']
 
-  // Routes if the currentUser exists
-  if (currentUser._id) {
+  // Jumps to the next route if the user is _definitely_ not authenticated
+  if (!token) {
+    return next()
+  } else if (currentUser._id) {
+    // Routes if the currentUser exists
     next()
   } else {
-    // TODO - loading spinner
     // Requests currentUser and redirects upon Promise resolution
-    store.dispatch('auth/fetchUserProfile').then(() => { next() })
+    store.dispatch('auth/fetchUserProfile')
+    .then(() => { next() })
+    .catch(() => { next() })
   }
 }
 
