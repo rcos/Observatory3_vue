33871418f713a8d713ac27afc97d16652fe21b39
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-15 12:03:50 -0500

    Added annotaion for GitHub issue #28

--- src/containers/main_sponsors/components/layout.vue
+++ src/containers/main_sponsors/components/layout.vue
@@ -7,11 +7,12 @@
         <p class="lead">TODO - implement sponsors page</p>
       </div>
     </div>
-
-</div>
+  </div>
 </template>
 
 <script>
+// TASK - implement sponsors page
+// https://github.com/rcos/observatory-client/issues/28
 export default {
   name: 'main_sponsors'
 }
