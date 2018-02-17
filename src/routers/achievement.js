129567759982666497bf3f2c93bdafb7ae2b230e
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-17 18:57:16 -0500

    Refined router implementation - split into module-specific router files

---
+++ src/routers/achievement.js
@@ -0,0 +1,12 @@
+// Achievement Containers
+import AchievementList from '@/containers/achievement_list'
+
+const AchievementListRoute = {
+  path: '/achievements',
+  name: 'achievement_list',
+  component: AchievementList
+}
+
+export default [
+  AchievementListRoute
+]
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,171 +1,25 @@
-import Vue from 'vue'
 import Router from 'vue-router'
-
-// Router Middleware
 import Middleware from './middleware'
 
-// Main Containers
-import MainHome from '@/containers/main_home'
-import MainSponsors from '@/containers/main_sponsors'
-import MainAttend from '@/containers/main_attend'
+// Import routes
+import AdminRoutes from './admin'
+import AuthRoutes from './auth'
+import BlogRoutes from './blog'
+import MainRoutes from './main'
+import MentorRoutes from './mentor'
+import ProjectRoutes from './project'
+import UserRoutes from './user'
 
-// Achievement Containers
-import AchievementList from '@/containers/achievement_list'
-
-// Project Containers
-import ProjectList from '@/containers/project_list'
-import ProjectNew from '@/containers/project_new'
-import ProjectShow from '@/containers/project_show'
-import ProjectEdit from '@/containers/project_edit'
-
-// Admin Containers
-import AdminAttendance from '@/containers/admin_attendance'
-import AdminSettings from '@/containers/admin_settings'
-import AdminUsers from '@/containers/admin_users'
-
-// Auth Containers
-import AuthLogin from '@/containers/auth_login'
-import AuthRegister from '@/containers/auth_register'
-
-// User Containers
-import UserList from '@/containers/user_list'
-import UserShow from '@/containers/user_show'
-import UserSettings from '@/containers/user_settings'
-
-// Mentor Containers
-import MentorVerify from '@/containers/mentor_verify'
-import MentorSmallGroup from '@/containers/mentor_small_group'
-
-// Blog Containers
-import BlogList from '@/containers/blog_list'
-import BlogNew from '@/containers/blog_new'
-import BlogEdit from '@/containers/blog_edit'
-import BlogShow from '@/containers/blog_show'
-
-Vue.use(Router)
-
-// TODO - split this into smaller, module-specific route definitions
+// Instantiates new Vue-Router instance
 let router = new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: MainHome
-    },
-    {
-      path: '/sponsors',
-      name: 'Sponsors',
-      component: MainSponsors
-    },
-    {
-      path: '/achievements',
-      name: 'Achievements',
-      component: AchievementList
-    },
-    {
-      path: '/projects',
-      name: 'Project List',
-      component: ProjectList
-      // beforeEnter: Middleware.ensureAuthenticated
-    },
-    {
-      path: '/projects/new',
-      name: 'Project New',
-      component: ProjectNew
-      // beforeEnter: Middleware.requireAuth
-    },
-    {
-      path: '/projects/:id',
-      name: 'Project Show',
-      component: ProjectShow,
-      props: true
-    },
-    {
-      path: '/projects/:id/edit',
-      name: 'Project Edit',
-      component: ProjectEdit,
-      props: true
-      // beforeEnter: Middleware.requireAuth
-    },
-    {
-      path: '/auth/login',
-      name: 'Login',
-      component: AuthLogin
-    },
-    {
-      path: '/auth/register',
-      name: 'Register',
-      component: AuthRegister
-    },
-    {
-      path: '/users',
-      name: 'User List',
-      component: UserList
-    },
-    {
-      path: '/users/:id',
-      name: 'User Show',
-      component: UserShow,
-      props: true
-    },
-    {
-      path: '/settings',
-      name: 'User Settings',
-      component: UserSettings
-    },
-    {
-      path: '/mentor/verify',
-      name: 'Mentor Verify',
-      component: MentorVerify
-    },
-    {
-      path: '/mentor/small_group',
-      name: 'Mentor Small Group',
-      component: MentorSmallGroup
-    },
-    {
-      path: '/blogs',
-      name: 'Blog List',
-      component: BlogList
-    },
-    {
-      path: '/blogs/new',
-      name: 'Blog New',
-      component: BlogNew
-    },
-    {
-      path: '/blogs/:id/edit',
-      name: 'Blog Edit',
-      component: BlogEdit,
-      props: true
-    },
-    {
-      path: '/blogs/:id',
-      name: 'Blog Show',
-      component: BlogShow,
-      props: true
-    },
-    {
-      path: '/admin/attendance',
-      name: 'Admin Attendance',
-      component: AdminAttendance
-    },
-    {
-      path: '/admin/users',
-      name: 'Admin Users',
-      component: AdminUsers
-    },
-    {
-      path: '/admin/settings',
-      name: 'Admin Settings',
-      component: AdminSettings
-    },
-    {
-      path: '/main/attend',
-      name: 'Main Attendance',
-      component: MainAttend
-    }
-
+    ...AdminRoutes,
+    ...AuthRoutes,
+    ...BlogRoutes,
+    ...MainRoutes,
+    ...MentorRoutes,
+    ...ProjectRoutes,
+    ...UserRoutes
   ]
 })
 
