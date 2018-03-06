2c8770a226988051164bf8fd9ccc6e545c956060
JoAnna Detweiler <joannadetweiler@gmail.com>
2018-03-06 17:23:04 -0500

    Added description and mozilla

--- src/containers/main_sponsors/components/layout.vue
+++ src/containers/main_sponsors/components/layout.vue
@@ -5,7 +5,10 @@
     <div class="row">
       <div class="col-lg-12">
       	<p class="lead"> Below are our sponsors that have supported our open source projects!</p>
-        <a href="https://securelb.imodules.com/s/1225/giving/index.aspx?sid=1225&gid=1&pgid=1947&cid=4524&appealcode=WGAFP&_ga=1.191668650.1206279174.1408978267" class="btn btn-primary btn-warning btn-lg gradient" role="button" target="_blank">Donate</a>
+
+
+        <!-- DONATE BUTTON  -->
+        <a href="https://securelb.imodules.com/s/1225/giving/index.aspx?sid=1225&gid=1&pgid=1947&cid=4524&appealcode=WGAFP&_ga=1.191668650.1206279174.1408978267" class="btn btn-primary btn-lg gradient" role="button" target="_blank" >Donate</a>
 
         <br><br>
 
@@ -16,7 +19,12 @@
               <br>
             </div>
             <div>
-              {{ item.name }}
+              <h1>
+                {{ item.name }}
+              </h1>
+              <p>
+                {{ item.description }}
+              </p>
             </div>
       			
       		</li>
@@ -35,11 +43,12 @@ export default {
   data () {
     return {
       sponsors: [
-        { name: 'RedHat', logo: 'http://logo-load.com/uploads/posts/2016-02/1456126060_logo-red-hat.png', alt: 'Red Hat Linux', url: 'https://www.redhat.com/', description: '' },
-        { name: 'Open Source Initiative', logo: 'https://opensource.org/files/osi_standard_logo.png', alt: 'Open Source Initiative', url: 'https://opensource.org/', description: '' },
-        { name: 'NSF HFOSS', logo: 'http://www.hfoss.org/uploads/images/allgray_notrinity.gif', alt: 'HFOSS', url: 'http://www.hfoss.org/index.php/contact-us', description: '' },
-        { name: 'Microsoft', logo: 'https://mtaiit.com/imgs/microsoft.png', alt: 'Microsoft', url: 'https://www.microsoft.com/en-us/', description: '' },
-        { name: 'Google', logo: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/6466217/fixed-google-logo-font.png', alt: 'Google', url: 'https://www.google.com/', description: '' }
+        { name: 'RedHat', logo: 'http://logo-load.com/uploads/posts/2016-02/1456126060_logo-red-hat.png', alt: 'Red Hat Linux', url: 'https://www.redhat.com/', description: 'RedHat has funded student projects and learning workshops, including Introduction to Open Source Software.' },
+        { name: 'Mozilla', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Mozilla_logo.svg/2000px-Mozilla_logo.svg.png', alt: 'Mozilla', url: 'https://www.mozilla.org/en-US/foundation/', description: 'Helped with leadership program for mentors.' },
+        { name: 'Open Source Initiative', logo: 'https://opensource.org/files/osi_standard_logo.png', alt: 'Open Source Initiative', url: 'https://opensource.org/', description: 'Educational Institution Member that has provided opportunities for students.' },
+        { name: 'NSF HFOSS', logo: 'http://www.hfoss.org/uploads/images/allgray_notrinity.gif', alt: 'HFOSS', url: 'http://www.hfoss.org/index.php/contact-us', description: 'Affliated member with Trinity, Connecticut College and Wesleyan University' },
+        { name: 'Microsoft', logo: 'https://mtaiit.com/imgs/microsoft.png', alt: 'Microsoft', url: 'https://www.microsoft.com/en-us/', description: 'Partially Funded Student projects (through Microsoft employee program).' },
+        { name: 'Google', logo: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/6466217/fixed-google-logo-font.png', alt: 'Google', url: 'https://www.google.com/', description: 'Donated MAGPis and Cardboard VR.' }
       ]
     }
   }
