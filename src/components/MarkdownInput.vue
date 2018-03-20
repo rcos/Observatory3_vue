821ae9bc22f6e038321ac7963c89381ebadba81a
Huanzhi Zhu <justicepigzhz@gmail.com>
2018-03-20 18:13:08 -0400

    update package for markdown editor && init MarkdownInput component

---
+++ src/components/MarkdownInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <mavon-editor style="height: 100%"></mavon-editor>
+</template>
+
+<!-- // // // //  -->
+
+<script>
+import mavonEditor from 'mavon-editor'
+
+export default {
+  name: 'MarkdownInput',
+  components: {
+    mavonEditor
+  },
+  data () {
+    return { value: '' }
+  }
+}
+</script>
--- src/containers/blog_new/components/layout.vue
+++ src/containers/blog_new/components/layout.vue
@@ -3,6 +3,7 @@
   <div class="container">
     <PageHeader title="New Blog Post"/>
     <BlogForm :onSubmit="create" />
+    <MarkdownInput />
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 
 <script>
 import BlogForm from '@/components/BlogForm'
+import MarkdownInput from '@/components/MarkdownInput'
 import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'layout',
   components: {
-    BlogForm
+    BlogForm,
+    MarkdownInput
   },
   methods: mapActions({
     create: 'project/create'
