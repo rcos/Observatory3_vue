e24c0d6ffabad197b52f6ea72c162c51327b4e89
Huanzhi Zhu <justicepigzhz@gmail.com>
2018-03-20 16:46:34 -0400

    finished supplement ProjectSelect component && finished Blog-new layout

--- src/containers/blog_new/components/layout.vue
+++ src/containers/blog_new/components/layout.vue
@@ -2,60 +2,23 @@
 <template>
   <div class="container">
     <PageHeader title="New Blog Post"/>
-
-    <form>
-      <div class="form-group">
-        <label>Title: </label>
-        <input type="title" class = "form-control" id="blogFormControlTitle">
-      </div>
-
-      <ProjectSelect />
-
-      <div class="form-group">
-       <label>Tags: </label>
-       <input-tag placeholder="Project Tech" :tags="tags" />
-       <ul class="list-group"></ul>
-      </div>
-
-      <div class="form-group">
-        <label>Body: </label>
-        <textarea class="form-control" id="blogFormControlBody" rows="5"></textarea>
-      </div>
-
-      <div class="col-lg-6">
-        <FormSubmit class='w-100' :click="onSubmit"/>
-      </div>
-    </form>
-
+    <BlogForm :onSubmit="create" />
   </div>
 </template>
 
  <!-- // // // //  -->
 
 <script>
-import InputTag from 'vue-input-tag'
-import PageHeader from '@/components/PageHeader'
-import FormSubmit from '@/components/FormSubmit'
-import ProjectSelect from '@/components/ProjectSelect'
+import BlogForm from '@/components/BlogForm'
 import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'layout',
   components: {
-    InputTag,
-    PageHeader,
-    FormSubmit,
-    ProjectSelect
-  },
-  data () {
-    return {
-      tags: [
-        'HTML', 'JS', 'Bootstrap'
-      ]
-    }
+    BlogForm
   },
   methods: mapActions({
-    onSubmit: 'project/create'
+    create: 'project/create'
   }),
   computed: mapGetters({
 
