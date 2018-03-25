2b32aeb5b66b9d65cecb8f516621ba45a8cb285b
Alexander Schwartzberg <aeksco@gmail.com>
2018-03-25 16:16:23 -0400

    Added constants.js, setUserRole to User Vuex store

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,7 +1,6 @@
-import { $GET } from '@/store/lib/helpers'
+import { $GET, $POST } from '@/store/lib/helpers'
 import { PAGINATION_ACTIONS, FILTER_ACTIONS } from '@/store/lib/mixins'
-
-const API_ROOT = '/api/users'
+import { API_ROOT, SET_ROLE_NOTIFICATIONS } from './constants'
 
 // // // //
 
@@ -31,6 +30,26 @@ export default {
     })
   },
 
+  // fetchAdminCollection
+  // Admin-only user collection for /admin/users
+  fetchAdminCollection: ({ commit, state, rootGetters }) => {
+    commit('fetching', true)
+
+    // Fetches either active or inactive users
+    let apiRoute = API_ROOT + '/adminstats'
+
+    // Fetches Collection from the server
+    $GET(apiRoute, { token: rootGetters['auth/token'] })
+    .then((json) => {
+      commit('fetching', false)
+      commit('collection', json)
+    })
+    .catch((err) => {
+      commit('fetching', false)
+      throw err // TODO - better error handling
+    })
+  },
+
   // fetchUser
   // Fetches an individual user from the server
   fetchUser ({ store, commit, rootGetters }, userID) {
@@ -43,6 +62,28 @@ export default {
     .catch((err) => {
       commit('fetching', false)
       throw err // TODO - better error handling
+    })
+  },
+
+  // setUserRole
+  // Updates an individual User's role
+  // @permissions Admin
+  setUserRole ({ commit, rootGetters }, { userID, role }) {
+    commit('fetching', true)
+
+    // POST /api/users/:id/role
+    $POST(API_ROOT + '/' + userID + '/role', {
+      token: rootGetters['auth/token'],
+      body: { role }
+    })
+    .then((json) => {
+      commit('notification/add', SET_ROLE_NOTIFICATIONS.SUCCESS, { root: true })
+      commit('fetching', false)
+    })
+    .catch((err) => {
+      commit('notification/add', SET_ROLE_NOTIFICATIONS.ERROR, { root: true })
+      commit('fetching', false)
+      throw err
     })
   }
 }
---
+++ src/store/user/constants.js
@@ -0,0 +1,15 @@
+export const API_ROOT = '/api/users'
+
+// Notifications
+export const SET_ROLE_NOTIFICATIONS = {
+  SUCCESS: {
+    message: 'Successfully updated User role',
+    context: 'success',
+    dismissible: true
+  },
+  ERROR: {
+    message: 'Error updating User role',
+    context: 'warning',
+    dismissible: true
+  }
+}
