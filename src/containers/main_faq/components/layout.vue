199ef659cd79e0682c1b064acbce92f4a8328dee
Houming Zhu <zhm_andrew@erratics-172.dynamic.rpi.edu>
2018-04-10 16:57:27 -0400

    Added a github intro link to FAQ page

--- src/containers/main_faq/components/layout.vue
+++ src/containers/main_faq/components/layout.vue
@@ -2,14 +2,8 @@
   <div class="container">
     <PageHeader title="FAQ" />
     <p class="lead">
-      Frequently asked questions will be answered here!
+       <a href="https://github.com/rcos/intro" target='_blank'>RCOS Intro</a>
     </p>
-
-   
-
-
-    
-   
   </div>
 </template>
 
