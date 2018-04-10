d2b41f03a93e777ffc9f2e785e9500c601a4ca00
charlie <schmic6@rpi.edu>
2018-04-10 17:05:10 -0400

    Added a PageHeader to the admin attendance page

--- src/containers/admin_attendance/components/layout.vue
+++ src/containers/admin_attendance/components/layout.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div class="container">
-    <h2>Full Group Attendance</h2>
+    <PageHeader title="Attendance Control Panel" />
     <GenerateAttendance/>
     <hr>
     <h3>Attendance Days:</h3>
