af3ffcdc8bfc7c70f70b1eeb06ff30cf0f766f31
madebymuiz <muiz.reform@gmail.com>
2018-04-10 16:14:22 -0400

    Create & update in store/blog & store/achievements

--- src/store/blog/actions.js
+++ src/store/blog/actions.js
@@ -1,4 +1,4 @@
-import { $GET } from '@/store/lib/helpers'
+import { $GET, $POST, $PUT } from '@/store/lib/helpers'
 
 const API_ROOT = '/api/posts'
 
@@ -32,9 +32,40 @@ export default {
       throw err
     })
   },
-  create ({ commit }) {
+  create ({ commit, rootGetters }) {
+    $POST(API_ROOT, {
+      token: rootGetters['auth/token'],
+      body: {
+        title: 'sample-blog-create',
+        description: 'sample123',
+        date: new Date ()
+      }
+    })
+    .then((json) => {
+      console.log('Blog Create [Done]')
+      console.log(json)
+    })
+    .catch((err) => {
+      console.log("Error in blog create:\n" + err)
+    })
   },
-  update ({ commit }) {
+  update ({ commit, rootGetters }, blogId) {
+    $PUT(API_ROOT + '/' + blogId, {
+      token: rootGetters['auth/token'],
+      body: {
+        title: 'blog update',
+        description: 'sample blog update'
+      }
+    })
+    .then((json) => {
+      console.log('Successfully updated blog ID: ' + blogId)
+      console.log( json )
+    })
+    .catch((err) => {
+      console.log('Error in updating blog ID: ' + blogId)
+      console.log( err )
+      throw err
+    })
   },
   destroy ({ commit }) {
   }
