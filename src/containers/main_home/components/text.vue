db98ef13a1979ba2b6e776ac62b6d39e0c85dc7e
Houming Zhu <zhm_andrew@erratics-172.dynamic.rpi.edu>
2018-04-21 16:38:37 -0400

    modified auth register page

--- src/containers/main_home/components/text.vue
+++ src/containers/main_home/components/text.vue
@@ -11,7 +11,7 @@
     <h4>Details</h4>
     <p>Students can participate in RCOS for course credit or for a stipend. At the beginning of each semester, the Internal Advisory Board will review project proposals and decide which projects should receive financial support.</p>
     <h4>Contact</h4>
-    <p>For information and inquiry, please contact us via e-mail at <a href="mailto:coordinators@rcos.io">coordinators@rcos.io.</a>.</p>
+    <p>For information and inquiry, please contact us via e-mail at <a href="mailto:coordinators@rcos.io">course coordinator.</a>.</p>
     <h4>Donations</h4>
     <p>RCOS uses funds to financially support open source software development. Follow this link if you can help us out by donating!</p>
   </div>
