b80c77a33225469169f6494a63086d00513c6811
Alexander Schwartzberg <aeksco@gmail.com>
2018-07-23 13:19:13 -0400

    Minor bug fix in ProjectSelect component

--- src/components/ProjectSelect.vue
+++ src/components/ProjectSelect.vue
@@ -2,7 +2,7 @@
   <div class="form-group">
     <label>Project</label>
     <select class="form-control" id="blogFormControlSelect">
-      <option :value="project._id" v-for="project in projectCollection">
+      <option :value="project._id" v-for="project in projectCollection" :key="project._id">
         {{ project.name }}
       </option>
     </select>
