2c1c9e31d0559cd8c84aac1f35e51c34c79ac5a8
Alexander Schwartzberg <aeksco@gmail.com>
2018-07-23 12:42:16 -0400

    Added attendanc module directory

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
-import project from './project'
+import project from '@/modules/project/store'
 import user from './user'
 import smallgroup from './smallgroup'
 import notification from './notification'
-import attendance from './attendance'
+import attendance from '@/modules/attendance/store'
 import classyear from './classyear'
 
 Vue.use(Vuex)
