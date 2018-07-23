a42496fc0d48ceef6bf8491dde1c12568927ed4a
Alexander Schwartzberg <aeksco@gmail.com>
2018-07-23 12:54:17 -0400

    Removed unused markdown components & dependencies

--- src/config/vue.js
+++ src/config/vue.js
@@ -3,8 +3,6 @@ import Router from 'vue-router'
 import Vuex from 'vuex'
 import Meta from 'vue-meta'
 import BootstrapVue from 'bootstrap-vue'
-import mavonEditor from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -22,6 +20,3 @@ Vue.use(Meta)
 
 // BootstrapVue configuration
 Vue.use(BootstrapVue)
-
-// mavon-editor
-Vue.use(mavonEditor)
